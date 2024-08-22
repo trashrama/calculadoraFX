ad9dfcf2972372528eaa1aacac0a68caa031cced
trashrama <canaisdosantiago@gmail.com>
2024-08-22 11:20:37 -0300

    kdasdasdjasdjasdjasdjaksjd

--- calculadora/src/main/java/com/projetossala/calculadora/HelloController.java
+++ calculadora/src/main/java/com/projetossala/calculadora/HelloController.java
@@ -6,8 +6,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Set;
 
 public class HelloController {
 
@@ -16,19 +20,24 @@ public class HelloController {
     private TextField construcao;
 
     @FXML
-    private boolean verifica(String c){
-        boolean quebrou = false;
-        String[] simbolosProibidos = {"+","-", "*", "/"}; // consertar para **
-        if(construcao.getLength()>0){
-            for(String s: simbolosProibidos){
-                if (s.equals(construcao.getText().substring(construcao.getLength()-1))){
-                    quebrou = true;
-                }
-            }
-            if (construcao.getText().substring(construcao.getLength()-1).equals(c))
-                quebrou = true;
+    private Label resultado;
 
-        }
+    @FXML
+    private boolean verifica(String c){
+
+        // também não pode deixar começar com algum dos simbolos proibidos
+
+        boolean quebrou = false;
+        Set<Character> simbolosProibidos = Set.of('+','-', 'x', '/'); // consertar para **
+        char ant, now;
+        if(c.length()>2){
+            ant = c.charAt(c.length()-2);
+            now = c.charAt(c.length()-1);
+
+            if (simbolosProibidos.contains(ant) && simbolosProibidos.contains(now))
+                quebrou = true;
+         }
+
         return quebrou;
 
     }
@@ -36,11 +45,17 @@ public class HelloController {
     @FXML
     private void add(ActionEvent event) {
         Button botaoClicado = (Button) event.getSource();
-        if (!verifica(botaoClicado.getText()))
+        if (!verifica(construcao.getText()+botaoClicado.getText()))
             construcao.setText(construcao.getText()+botaoClicado.getText());
     }
-
+    @FXML
     private void del(ActionEvent event){
         construcao.setText(construcao.getText().substring(0, construcao.getLength()-2));
+    }
+    @FXML
+    private void calcular(ActionEvent event) throws ScriptException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("js");
+        resultado.setText((String) engine.eval(construcao.getText()));
     }
 }
--- calculadora/src/main/java/module-info.java
+++ calculadora/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.projetossala.calculadora {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.scripting;
 
 
     opens com.projetossala.calculadora to javafx.fxml;
